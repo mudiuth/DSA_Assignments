8af32592672385583243f95ed1e42dd293035a8e
MrSIr <mrsir@kali.uthman.com>
2025-03-15 23:43:57 +0300

    started working on the sacco project

---
+++ Project_Work/my_sacco_project.c
@@ -0,0 +1,31 @@
+/* am to mamke a data structure implementation sacco that will have to provede financial services to the farmers
+these services will include;
+withdrawals: a farmer withdraws the amount that he has so the function shud fst chdck thebalance and also verify the acct 
+depositing: the farmer shud be able to top up the money she or he is having on their acct and this is the depositing process.. all that is required is to check the availability of the acct. all deposit shud store the time stamps 
+check balance: this function shud be able to return the actual balance the farmer has withou any error
+
+below is the data needed 
+AcctName
+AcctBalance
+AcctNo_
+AcctPin
+DepositedAmount
+
+*/
+#include <stdio.h>
+
+MainMenu
+
+
+
+int main(){
+char
+
+printf("welcome to the MINER SAVINGS SACCO \n big from nothing");
+printf("\nPlease select an option;")
+
+
+
+
+	return 0;
+}
